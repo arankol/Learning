9b73204bab44316b52bb3d74dd8892834e29e104
arankol <arankol2@yandex.ru>
2022-10-10 00:44:48 +0300

    lab2.sell

---
+++ stock/admin.py
@@ -0,0 +1,40 @@
+from stock.models import (Stock, Currency, Account,
+                          AccountCurrency, AccountStock)
+from django.contrib import admin
+
+from stock.models import Stock, Currency
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("ticker", "name", "description")
+    pass
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
+@admin.register(AccountCurrency)
+class AccountCurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccountStock)
+class AccountStockAdmin(admin.ModelAdmin):
+    pass
+
+class AccountCurrencyInline(admin.TabularInline):
+    model = AccountCurrency
+
+
+class AccountStockInline(admin.TabularInline):
+    model = AccountStock
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [AccountCurrencyInline, AccountStockInline]
+
+
+
+
+
+
